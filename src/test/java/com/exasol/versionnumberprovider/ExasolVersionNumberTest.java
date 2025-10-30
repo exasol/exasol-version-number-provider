802d9199ec2c8fe93f14def9c1896800fc1d491a
Christoph Pirkl <christoph.pirkl@exasol.com>
2025-10-30 13:37:01 +0100

    Adapt to latest Exasol version

--- src/test/java/com/exasol/versionnumberprovider/ExasolVersionNumberTest.java
+++ src/test/java/com/exasol/versionnumberprovider/ExasolVersionNumberTest.java
@@ -19,8 +19,11 @@ class ExasolVersionNumberTest {
     private static final ExasolVersionNumber RELEASE_601 = new ExasolVersionNumber("6.0.1");
     private static final ExasolVersionNumber RELEASE_600D1 = new ExasolVersionNumber("6.0.0-d1");
     private static final ExasolVersionNumber RELEASE_602 = new ExasolVersionNumber("6.0.2");
+    private static final ExasolVersionNumber RELEASE_800 = new ExasolVersionNumber("8.0.0");
+    private static final ExasolVersionNumber RELEASE_82913 = new ExasolVersionNumber("8.29.13");
+    private static final ExasolVersionNumber RELEASE_202513 = new ExasolVersionNumber("2025.1.3");
     private static final List<ExasolVersionNumber> RELEASES = List.of(RELEASE_600, RELEASE_700, RELEASE_610,
-            RELEASE_601, RELEASE_600D1, RELEASE_602);
+            RELEASE_601, RELEASE_600D1, RELEASE_602, RELEASE_800, RELEASE_82913, RELEASE_202513);
 
     @Test
     void testNotEquals() {
@@ -52,12 +55,23 @@ class ExasolVersionNumberTest {
     @Test
     void testSort() {
         assertThat(RELEASES.stream().sorted().collect(Collectors.toList()),
-                contains(RELEASE_600, RELEASE_600D1, RELEASE_601, RELEASE_602, RELEASE_610, RELEASE_700));
+                contains(RELEASE_600, RELEASE_600D1, RELEASE_601, RELEASE_602, RELEASE_610, RELEASE_700, RELEASE_800,
+                        RELEASE_82913, RELEASE_202513));
     }
 
     @Test
     void testToString() {
         assertThat(RELEASE_600.toString(), equalTo("6.0.0"));
+    }
+
+    @Test
+    void testMajor() {
+        assertThat(RELEASE_202513.getMajorVersion(), equalTo(2025));
+    }
+
+    @Test
+    void testMinor() {
+        assertThat(RELEASE_202513.getMinorVersion(), equalTo(1));
     }
 
     @Test

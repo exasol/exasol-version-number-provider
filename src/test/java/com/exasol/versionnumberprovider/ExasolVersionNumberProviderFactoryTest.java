00218f5b56a04ad96a6cb191e8524a4e6a5a4a47
Christoph Pirkl <christoph.pirkl@exasol.com>
2025-10-30 13:45:55 +0100

    Adapt test

--- src/test/java/com/exasol/versionnumberprovider/ExasolVersionNumberProviderFactoryTest.java
+++ src/test/java/com/exasol/versionnumberprovider/ExasolVersionNumberProviderFactoryTest.java
@@ -4,18 +4,19 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-import java.io.IOException;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 class ExasolVersionNumberProviderFactoryTest {
     @Test
-    void test() throws IOException {
+    void test() {
         final ExasolVersionNumberProvider versionNumberProvider = new ExasolVersionNumberProviderFactory()
                 .buildExasolVersionNumberProvider();
         final List<String> allReleaseNumbers = versionNumberProvider.getAllReleaseNumbers();
-        assertAll(() -> assertThat(allReleaseNumbers, hasItems("6.2.0-d1", "7.0.0", "7.0.1", "7.0.2", "7.1.14")),
-                () -> assertThat(allReleaseNumbers, hasSize(greaterThanOrEqualTo(57))));
+        assertAll(
+                () -> assertThat(allReleaseNumbers,
+                        hasItems("6.2.0-d1", "7.0.0", "7.0.1", "7.0.2", "7.1.14", "8.29.11", "2025.1.3")),
+                () -> assertThat(allReleaseNumbers, hasSize(greaterThanOrEqualTo(101))));
     }
 }
